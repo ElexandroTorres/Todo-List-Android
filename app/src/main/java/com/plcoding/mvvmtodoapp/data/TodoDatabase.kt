c25f31150a70abfc8eb52a0afd32480574d9da7f
Elexandro Torres <elexandro.torres@gmail.com>
2022-09-09 22:42:31 -0300

    data layer complete

--- app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDatabase.kt
+++ app/src/main/java/com/plcoding/mvvmtodoapp/data/TodoDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [Todo::class],
+    entities = [Todo::class], //Tabelas no projeto
     version = 1
 )
 abstract class TodoDatabase: RoomDatabase() {
